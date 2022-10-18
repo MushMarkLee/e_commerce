52f40a1d511b2f72c4588f4b3107df1570ad3be5
MushMarkLee <74833351+MushMarkLee@users.noreply.github.com>
2022-10-18 14:43:37 +0300

    Update SideAni.js

--- src/components/SideAni/SideAni.js
+++ src/components/SideAni/SideAni.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import flower from '../../assets/flower.svg';
 import flower1 from '../../assets/flower1.svg';
-import flower2 from '../../assets/flower2.svg';
 
 const SideAni = () => {
 
@@ -10,15 +9,10 @@ const SideAni = () => {
         <React.Fragment>
             <div id='flowerdiv'>
                 <img src={flower} id='flower'/>
-
                 <img src={flower1} id='flower1'/>
-
-
             </div>
         </React.Fragment>
-
     )
-
 }
 
 export default SideAni
