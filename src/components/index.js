a47125c675b2f7084f6d5dd08c42d6b3bf340a3f
MushMarkLee <74833351+MushMarkLee@users.noreply.github.com>
2022-10-18 14:42:51 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,9 @@
 export  { default as Navbar} from './Navbar/Navbar';
 export { default as Products} from './products/Products';
-
 export { default as Cart} from './Cart/Cart';
 export { default as Checkout} from './CheckoutForm/Checkout/Checkout';
-
 export {default as Screen } from './Screen/Screen';
-
 export { default as MyButton } from './Button/Button';
-
 export { default as Review } from './Review/Review';
-
 export { default as About } from './About/About';
-
 export { default as ProductView } from './ProductView/ProductView';
